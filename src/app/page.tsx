613ae6f8c8f9535ea08b250f8c016a90f53b9387
shabik-ashab <shabik.ashab@gmail.com>
2024-06-12 14:16:59 +0600

    feat(ui): add tile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import { Header } from "@/components/Header";
+import { Tile } from "@/components/Tile";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main >
       <Header />
+      <div className=" p-10">
+        <Tile />
+      </div>
     </main>
   );
 }
