18c58bf830e15f2b291081c1572e321673171dc0
shabik-ashab <shabik.ashab@gmail.com>
2024-06-12 13:33:52 +0600

    feat: add navbar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,11 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui:{
+    themes: ['light', 'dark']
+  }
 };
 export default config;
